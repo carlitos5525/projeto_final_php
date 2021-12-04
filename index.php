c835e7e7f7011e2b0daa7cd904be1ff10d2a5322
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-12-04 12:47:40 -0300

    tratamento mensagens

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Projeto Final - PHP</title>
 </head>
 <body>
     <h1>Projeto Final</h1>
+
+    <?php
+    if (isset($_GET['msg']))
+	{
+		include_once 'mensagens.php';
+		echo validar_msg($_GET['msg']);
+	}
+	?>
+
     <h2>Utilize o formulário abaixo para entrar no sistema<h2>
 
         <form action="validar.php" method="post">
--- sistema/index.php
+++ sistema/index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Projeto Final - Area Restrita</title>
 </head>
 <body>
@@ -16,6 +17,15 @@
     <p>
         <a href="logout.php">Sair do Sistema<a>
     <p>
+
+    <?php  
+
+	if (isset($_GET['msg']))
+	{
+		include_once '../mensagens.php';
+		echo validar_msg($_GET['msg']);
+	}
+	?>
 
     <h3>Utilize o form abaixo para cadastrar<h3>
 
